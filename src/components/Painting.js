09bf2da3a3931b9d9e4add8f4c1a1a19e597daa2
Hallala <mateuszhallala@gmail.com>
2021-03-12 16:48:04 +0100

    oooft

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -3,9 +3,18 @@ import React from 'react'
 const Painting = (props) => {
     return (
         <div className="painting">
-            <img src={props.imageUrl} width="300px" height="auto" alt="generic text" />
-            <h3>{props.title}</h3>
-            <p>are you description?</p>
+            <img src={props.imageUrl} width="310px" height="auto" alt="generic text" />
+            <header>
+                <h3>"{props.title}"</h3>
+                <span>2017 | oil on canvas | 50x70cm | $360</span>
+            </header>
+            
+            <p>
+                are you description?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente enim quas 
+                temporibus tempora nihil explicabo, ex odit sint numquam asperiores quis rerum 
+                quisquam iusto hic soluta vero nemo iure ipsa.
+            </p>
         </div>
     )
 }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,19 +1,33 @@
 import * as React from "react";
 import Layout from "../components/Layout.js"
 // import { Link } from "gatsby";
+import AboutImageFull from "../images/About.png"
 
 // markup
 const App = () => {
   return (
       <Layout>
-        <h1>
+        <div className="about__text">
+         
+          <img src={AboutImageFull} className="about__image-full"></img>
+          
+          <h2>
             Patrycja Zych
-        </h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur ad cum hic molestias ipsum et fugit 
-            deleniti, inventore laborum facilis non ratione quisquam minima, nihil sint! Sed quaerat commodi, ab officiis 
-            maxime impedit eum numquam iure, tempora, magnam sunt?
-        </p>
+          </h2>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur ad cum hic molestias ipsum et fugit 
+              deleniti, inventore laborum facilis non ratione quisquam minima, nihil sint! Sed quaerat commodi, ab officiis 
+              maxime impedit eum numquam iure, tempora, magnam sunt?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur ad cum hic molestias ipsum et fugit 
+              deleniti, inventore laborum facilis non ratione quisquam minima, nihil sint!
+              Debitis pariatur ad cum hic molestias ipsum et fugit 
+              deleniti, inventore laborum facilis non ratione quisquam minima, nihil sint!
+          </p>
+
+          
+ 
+       
+        </div>        
       </Layout>
   )
 }
